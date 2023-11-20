234137fdfb386c8b2a6033c96b03505720828cbe
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-20 22:11:54 +0700

    Soal 1 (Bubble Sort)

---
+++ Searching.ts
@@ -0,0 +1,11 @@
+const dataKaryawan = (String[] args) {
+    int [] ={80, 65, 90, 75, 85, 70, 95, 60};
+    console.log("Sebelum bubble sort");
+
+    int [] ={60, 65, 70, 75, 80, 85, 90, 95};
+    console.log("Sesudah bubble sort");
+
+
+
+
+
