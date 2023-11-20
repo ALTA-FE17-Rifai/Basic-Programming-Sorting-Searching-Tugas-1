d50cce8d2f5de864a3455c2897d0128c0aafaf24
26ipay <149394246+26ipay@users.noreply.github.com>
2023-11-20 22:18:43 +0700

    Soal 2 (Binary Search)

---
+++ Sorting.ts
@@ -0,0 +1,13 @@
+const dataKaryawan = String {
+    { id: 101, nama: "Rani"},
+    { id: 203, nama: "Andi"},
+    { id: 305, nama: "Budi"},
+    { id: 410, nama: "Cindy"},
+    { id: 520, nama: "Dodi"},
+    { id: 635, nama: "Eka"},
+    { id: 742, nama: "Fani"},
+
+    cons targetId = 305
+
+    if (targetId = 0, = null)
+}
